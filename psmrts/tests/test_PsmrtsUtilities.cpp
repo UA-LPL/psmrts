4f20ee18bc280f88e39a49b2ca128cf596283658
Kyle A Becker <kabecker@arizona.edu>
2024-01-24 16:06:55 -0700

    PSMRTS Longitude Domain testing 360/180, GENERATE Output test, and PSMRTS 360/180 routines

---
+++ psmrts/tests/test_PsmrtsUtilities.cpp
@@ -0,0 +1,33 @@
+
+#include <psmrts_catch2_environment.hpp>
+
+#include <PsmrtsUtilities.hpp>
+
+TEST_CASE( "PSMRTS Longitude Domain Test - 360 to 180", "[utilities][longitude][values]" ) {
+
+    const double tolerance = 1.0e-6;
+
+    CHECK_THAT( psmrts::to180LongitudeDomain_d( 0.0 ), Catch::Matchers::WithinAbs( 0.0, tolerance ));
+    CHECK_THAT( psmrts::to180LongitudeDomain_d( 90.0 ), Catch::Matchers::WithinAbs( 90.0, tolerance ));
+    CHECK_THAT( psmrts::to180LongitudeDomain_d( 180.0 ), Catch::Matchers::WithinAbs( -180.0, tolerance )); // 180 -> -180
+    CHECK_THAT( psmrts::to180LongitudeDomain_d( 190.0 ), Catch::Matchers::WithinAbs( -170.0, tolerance ));
+    CHECK_THAT( psmrts::to180LongitudeDomain_d( 300.0 ), Catch::Matchers::WithinAbs( -60.0, tolerance ));
+    CHECK_THAT( psmrts::to180LongitudeDomain_d( 360.0 ), Catch::Matchers::WithinAbs( 0.0, tolerance ));
+    CHECK_THAT( psmrts::to180LongitudeDomain_d( 700.0 ), Catch::Matchers::WithinAbs( -20, tolerance ));
+
+
+}
+
+TEST_CASE( "PSMRTS Longitude Domain Test - 180 to 360", "[utilities][longitude][values]") {
+
+    const double tolerance = 1.0e-6;
+
+    CHECK_THAT( psmrts::to360LongitudeDomain_d( 0.0 ), Catch::Matchers::WithinAbs( 0.0, tolerance ));
+    CHECK_THAT( psmrts::to360LongitudeDomain_d( 180.0 ), Catch::Matchers::WithinAbs( 180.0, tolerance ));
+    CHECK_THAT( psmrts::to360LongitudeDomain_d( -170.0 ), Catch::Matchers::WithinAbs( 190.0, tolerance ));
+    CHECK_THAT( psmrts::to360LongitudeDomain_d( -90.0 ), Catch::Matchers::WithinAbs( 270.0, tolerance ));
+    CHECK_THAT( psmrts::to360LongitudeDomain_d( -60.0 ), Catch::Matchers::WithinAbs( 300.0, tolerance ));
+    CHECK_THAT( psmrts::to360LongitudeDomain_d( 1000.0 ), Catch::Matchers::WithinAbs( 280.0, tolerance ));
+    // 1000 -> 280? Expected for this value?
+
+}
--- tracers/naifdsk/tests/test_NaifEllipsoidShape.cpp
+++ tracers/naifdsk/tests/test_NaifEllipsoidShape.cpp
@@ -1,4 +1,4 @@
-#define CATCH_CONFIG_MAIN
+// #define CATCH_CONFIG_MAIN
 #include <psmrts_catch2_environment.hpp>
 
 #include <NaifEllipsoidShape.hpp>
@@ -16,7 +16,7 @@ TEST_CASE( "NaifEllipsoidShape Default Test", "[naif][ellipsiod][default]") {
   Eigen::Vector3d radii = t_ellipse.radii();
   CHECK( t_ellipse.a()            == radii[0] );
   CHECK( t_ellipse.b()            == radii[1] );
-  CHECK( t_ellipse.c()            == radii[1] );
+  CHECK( t_ellipse.c()            == radii[2] );
 
   Eigen::Vector3d vec;
   double radius    = 1.0;
@@ -28,4 +28,53 @@ TEST_CASE( "NaifEllipsoidShape Default Test", "[naif][ellipsiod][default]") {
   CHECK_THAT( vec[1], Catch::Matchers::WithinAbs(0.2418447626, tolerance ) );
   CHECK_THAT( vec[2], Catch::Matchers::WithinAbs( 0.7071067812, tolerance ) );
 
+  double n_lat;
+  double n_long;
+  double n_rad;
+  reclat_c( vec.data(), &n_rad, &n_long, &n_lat );
+  CHECK_THAT( n_lat, Catch::Matchers::WithinAbs(latitude, tolerance));
+  CHECK_THAT( n_long, Catch::Matchers::WithinAbs(longitude, tolerance ));
+  CHECK_THAT( n_rad, Catch::Matchers::WithinAbs(radius, tolerance ));
+}
+
+TEST_CASE( "GENERATE Output Test", "[generate]") {
+  
+  const double tolerance = 1.0e-6;
+
+  double lat_val = GENERATE( -90.0, -45.0, 0.0, 45.0, 90.0);
+  double long_val = GENERATE( -180.0, -90.0, 0.0, 45.0, 90.0, 180.0, 359.999);
+
+  if ( long_val > 180 ) { 
+    long_val -= 360.0;
+  }
+
+  naif::NaifEllipsoidShape t_ellipse;
+  CHECK( t_ellipse.a()            == 1.0 );
+  CHECK( t_ellipse.b()            == 1.0 );
+  CHECK( t_ellipse.c()            == 1.0 );
+
+  Eigen::Vector3d radii = t_ellipse.radii();
+  CHECK( t_ellipse.a()            == radii[0] );
+  CHECK( t_ellipse.b()            == radii[1] );
+  CHECK( t_ellipse.c()            == radii[2] );
+
+  INFO( "Lon/Lat = " << long_val << ", " << lat_val );
+
+  CHECK ( long_val == long_val );
+  CHECK ( lat_val == lat_val );
+
+  Eigen::Vector3d vec;
+  double radius = 1.0;
+  double latitude = lat_val * rpd_c();
+  double longitude = long_val * rpd_c();
+  latrec_c( radius, longitude, latitude, vec.data() );
+
+  
+  double n_lat;
+  double n_long;
+  double n_rad;
+  reclat_c( vec.data(), &n_rad, &n_long, &n_lat );
+  CHECK_THAT( n_lat, Catch::Matchers::WithinAbs(latitude, tolerance));
+  CHECK_THAT( n_long, Catch::Matchers::WithinAbs(longitude, tolerance ));
+  CHECK_THAT( n_rad, Catch::Matchers::WithinAbs(radius, tolerance ));
 }
