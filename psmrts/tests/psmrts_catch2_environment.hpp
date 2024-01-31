dce3be98d8ba21d3311abc73ae27c83b585b4c00
Kris J. Becker <kjbecker@arizona.edu>
2024-01-31 12:23:01 -0700

    Move psmrts_make_path() to PsmrtsUtitlities.hpp

--- psmrts/tests/psmrts_catch2_environment.hpp
+++ psmrts/tests/psmrts_catch2_environment.hpp
@@ -9,6 +9,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
+#include <PsmrtsUtilities.hpp>
+
 #include <spdlog/spdlog.h>
 
 /** Root path which is directory above ./build */
@@ -16,42 +18,19 @@ inline std::string psmrts_rootpath( ) {
   return ( std::string( ".." ) );
 }
 
-/**
- * @brief Constructs a path that is OS sensitive
- * 
- * @param directory    Top level directory 
- * @param pathpart     An optional subdirectory for file
- * @return std::string The constructed path
- */
-inline std::string psmrts_make_path( const std::string &directory, 
-                                      const std::string &pathpart = "" ) {
-
-  if ( pathpart.size() == 0 ) {
-    return ( directory );
-  }
-
-  if ( directory.size() == 0 ) {
-    return ( pathpart );
-  }
-
-  size_t dirlen = directory.size();
-  std::string dpathdelim = ( directory[dirlen-1] == '/' ) ? "" : "/";
-  return ( directory + dpathdelim + pathpart );
-}
-
-/** Returns directory to the main ./psmrts source directory with optional subdir/file */
-inline std::string psmrts_psmrts_path( const std::string &subpart = "" ) {
-  return ( psmrts_make_path( psmrts_make_path( psmrts_rootpath(), "psmrts" ), subpart ) );
+/** Returns directory to the main ./psmrts system source directory with optional subdir/file */
+inline std::string psmrts_system_path( const std::string &subpart = "" ) {
+  return ( psmrts::psmrts_make_path( psmrts::psmrts_make_path( psmrts_rootpath(), "psmrts" ), subpart ) );
 }
 
 /** Returns directory to psmrts data directory with an optional subdir/file */
 inline std::string psmrts_data_path( const std::string &subpart = "" ) {
-  return ( psmrts_make_path( psmrts_psmrts_path( "data" ), subpart ) );
+  return ( psmrts::psmrts_make_path( psmrts_system_path( "data" ), subpart ) );
 }
 
 /** Returns directory to the main ./tracers source directory with optional subdir/file */
 inline std::string psmrts_tracers_path( const std::string &subpart = "" ) {
-  return ( psmrts_make_path( psmrts_make_path( psmrts_rootpath(), "tracers" ), subpart ) );
+  return ( psmrts::psmrts_make_path( psmrts::psmrts_make_path( psmrts_rootpath(), "tracers" ), subpart ) );
 }
 
 #endif
