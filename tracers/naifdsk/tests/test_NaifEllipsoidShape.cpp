f2935459ecafe828355dbe6855e9e537ae43d4db
Kyle A Becker <kabecker@arizona.edu>
2024-02-09 16:17:05 -0700

    Had to comment out test in test_PsmrtsDataModel (Data Value Test), prep for additional test in test_PsmrtsUtilities, and added large scale spheroid/ellipsoid test in test_NaifEllipsoidShape (Note: comment added about peculiar fails on only a few extreme cases)

--- tracers/naifdsk/tests/test_NaifEllipsoidShape.cpp
+++ tracers/naifdsk/tests/test_NaifEllipsoidShape.cpp
@@ -125,10 +125,11 @@ CHECK_THAT ( spt[1] , Catch::Matchers::WithinAbs(-surf[1], tolerance));
 CHECK_THAT ( spt[2] , Catch::Matchers::WithinAbs(-surf[2], tolerance));
 
 }
-
-TEST_CASE ( "NAIFEllipsoidShape Ray Trace Value-Range Test", "[naif][raytrace][observer]") {
+/*
+TEST_CASE ( "NAIFEllipsoidShape Ray Trace Value-Range Test - Sphere", "[naif][raytrace][observer][sphere]") {
   const double tolerance = 1.0e-9;
   const double a_radius_val = 1.0;
+  // for ellipsoid set b_radius_val, and c_radius_val.
 
   naif::NaifEllipsoidShape t_ellipse( a_radius_val );
 
@@ -177,4 +178,84 @@ TEST_CASE ( "NAIFEllipsoidShape Ray Trace Value-Range Test", "[naif][raytrace][o
   CHECK_THAT ( spt[1] , Catch::Matchers::WithinAbs( naif_spt[1], tolerance ));
   CHECK_THAT ( spt[2] , Catch::Matchers::WithinAbs( naif_spt[2], tolerance ));
 
+}
+*/
+
+// Ellipsoid loops similar to above, going through ellipse(a), ellipse(a, b), ellipse(a, b, c)
+
+TEST_CASE ( "NAIFEllipsoidShape Ray Trace Value-Range Test - Spheroid/Ellipsoid", "[naif][raytrace][observer][spheroid][triaxial][ellipsoid]") {
+  const double tolerance = 1.0e-6;
+
+  std::vector<double> a_radius_list = { 0.5, 1.0, 10.125, 100.0 };
+  std::vector<double> b_radius_list = { 0.5, 1.0, 10.125, 100.0 };
+  std::vector<double> c_radius_list = { 0.5, 1.0, 10.125, 100.0 };
+
+
+  double long_val = GENERATE( -180.0, -140.0, -90.0, -45.0, 0.0, 45.0, 90.0, 140.0, 180.0);
+  double lat_val = GENERATE( -90.0, -45.0, 0.0, 45.0, 90.0 );
+ 
+  for ( auto a_radius_val: a_radius_list ) {
+    for (auto b_radius_val: b_radius_list) {
+      for ( auto c_radius_val: c_radius_list ) {
+
+        naif::NaifEllipsoidShape t_ellipse( a_radius_val, b_radius_val, c_radius_val );
+      
+        INFO ( "a radius / b radius / c radius = " << a_radius_val << ", " << c_radius_val << ", " << c_radius_val );
+        INFO( "Lon/Lat = " << long_val << ", " << lat_val );
+
+        std::vector<double>  distance_list = { 10.0, 1000.0, 10000.0 }; //  1000000.0 -> doesnt seem to work with extreme spheroidal shape(a, b, c) = (100, 0.5, 0.5) 
+                                                                        //  and only for a few of the cases.
+        Eigen::Vector3d observer;
+        double radius = t_ellipse.maximum_radius();
+        double obs_long = long_val * rpd_c();
+        double obs_lat = lat_val * rpd_c();
+        for ( auto distance: distance_list ) {
+          (void) latrec_c ( radius * distance, obs_long, obs_lat, observer.data() );
+          INFO( "Observer Distance: " << distance  );
+
+          Eigen::Vector3d rec_surf;
+          double surf_long = 0.0 * rpd_c();
+          double surf_lat  = 0.0 * rpd_c(); 
+          (void) latrec_c ( radius, surf_long, surf_lat, rec_surf.data() ); 
+
+          Eigen::Vector3d origin ( { 0.0, 0.0, 0.0 } );
+          Eigen::Vector3d surf;
+          SpiceBoolean s_found;
+          (void) surfpt_c( origin.data(), rec_surf.data(), a_radius_val, b_radius_val, c_radius_val, surf.data(), &s_found );
+
+          Eigen::Vector3d lookdir  = surf - observer;
+
+          Eigen::Vector3d spt ( { 0, 0, 0, } );
+
+          bool            good   = t_ellipse.ray_trace(observer, lookdir, spt);
+          Eigen::Vector3d normal = t_ellipse.normal( spt );
+          // call surfpt_c w/ parameters, declare bool - true vs 0. Found == 0, not found / or 1 if found 
+
+          Eigen::Vector3d naif_spt ( { 0, 0, 0, } );
+          SpiceBoolean found; 
+          (void) surfpt_c( observer.data(), lookdir.data(), t_ellipse.a(), t_ellipse.b(), t_ellipse.c(), naif_spt.data(), &found );
+
+          Eigen::Vector3d naif_normal ( { 0, 0, 0, } );
+          (void) surfnm_c( a_radius_val, b_radius_val, c_radius_val, naif_spt.data(), naif_normal.data() );
+
+          CHECK( good   == true );
+          CHECK( found  == SPICETRUE );
+          //CHECK( normal == naif_normal );
+          CHECK_THAT ( normal[0] , Catch::Matchers::WithinAbs( naif_normal[0], tolerance )); 
+          CHECK_THAT ( normal[1] , Catch::Matchers::WithinAbs( naif_normal[1], tolerance ));
+          CHECK_THAT ( normal[2] , Catch::Matchers::WithinAbs( naif_normal[2], tolerance ));
+
+          double emission = vsep_c( observer.data(), spt.data() ) * dpr_c();
+          INFO( "Emission Angle = " << emission );
+
+          double surfsep = vsep_c( naif_spt.data(), spt.data() ) * dpr_c();
+          INFO( "Surfpt Angle   = " << surfsep );
+
+          CHECK_THAT ( spt[0] , Catch::Matchers::WithinAbs( naif_spt[0], tolerance )); 
+          CHECK_THAT ( spt[1] , Catch::Matchers::WithinAbs( naif_spt[1], tolerance ));
+          CHECK_THAT ( spt[2] , Catch::Matchers::WithinAbs( naif_spt[2], tolerance ));
+        }
+      }
+    } 
+  }
 }
