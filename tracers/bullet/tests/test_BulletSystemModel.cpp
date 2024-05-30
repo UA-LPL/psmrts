b50b5c1e9b8150e0ba62e238a0940fb1f0f4ee1b
Kris J. Becker <kjbecker@arizona.edu>
2024-05-30 13:41:35 -0700

    Add Bullet Catch2 tests

---
+++ tracers/bullet/tests/test_BulletSystemModel.cpp
@@ -0,0 +1,24 @@
+
+#include <psmrts_catch2_environment.hpp>
+
+#include <PsmrtsBulletMeshMap.hpp>
+#include <PsmrtsOBJAsset.hpp>
+
+
+TEST_CASE ( "Bullet System Info", "[bullet][system]" ) {
+  CHECK( psmrts::bullet::bt_MaxBodyParts()        == 1024 );
+  CHECK( psmrts::bullet::bt_MaxTrianglesPerPart() == 2097152 );
+
+  CHECK( psmrts::bullet::b3_MaxBodyParts()        == 1024 );
+  CHECK( psmrts::bullet::b3_MaxTrianglesPerPart() == 2097152 );
+
+  double *d_none = nullptr;
+  CHECK( psmrts::bullet::bt_type_code ( d_none ) == PHY_DOUBLE );
+
+  float  *f_none = nullptr;
+  CHECK( psmrts::bullet::bt_type_code ( f_none ) == PHY_FLOAT );  
+
+  int  *i_none = nullptr;
+  CHECK( psmrts::bullet::bt_type_code ( i_none ) == PHY_INTEGER);    
+
+}
--- tracers/bullet/tests/test_PsmrtsBulletMeshMap.cpp
+++ tracers/bullet/tests/test_PsmrtsBulletMeshMap.cpp
@@ -2,9 +2,11 @@
 #include <psmrts_catch2_environment.hpp>
 
 #include <PsmrtsBulletMeshMap.hpp>
+#include <PsmrtsOBJAsset.hpp>
 
-TEST_CASE ( "Bullet Mesh Map Test - Default Constructor", "[default][bullet][mapping]" ) {
-    psmrts::bullet::PsmrtsBulletMeshMap b_map;
+
+TEST_CASE ( "Bullet Mesh Map Test - Default Constructor", "[default][bullet][mesh]" ) {
+    psmrts::bullet::DefaultBulletMesh b_map;
 
     CHECK ( b_map.isValid() == false );
     CHECK ( b_map.name() == "BulletMesh" );
@@ -13,4 +15,23 @@ TEST_CASE ( "Bullet Mesh Map Test - Default Constructor", "[default][bullet][map
    CHECK ( b_map.mesh() == nullptr );
    CHECK ( b_map.toBullet()[0] == 0 );
 
+}
+
+TEST_CASE ( "Bullet Mesh Map Test - Small Dataset", "[bullet][mesh]" ) {
+
+    typedef psmrts::bullet::BulletInternalMeshModel BulletMeshData;
+    typedef psmrts::bullet::PsmrtsBulletMeshMap<BulletMeshData> BulletShape;
+
+    std::string objfile = psmrts_formats_path( "obj/data/bennu_20facets.obj" );
+    psmrts::PsmrtsOBJAsset t_loader( objfile );
+
+    BulletShape bt_mesh( t_loader );
+    CHECK ( bt_mesh.isValid() == true );
+    CHECK ( bt_mesh.name()    == t_loader.obj_source() );
+    CHECK ( bt_mesh.id()      == 0 );
+
+    CHECK ( bt_mesh.mesh_type() == "Bullet" );
+
+    CHECK ( bt_mesh.mesh() != nullptr );
+    CHECK ( bt_mesh.shape() != nullptr );
 }
