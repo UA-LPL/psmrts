6faba5b9e03ae0717f1597884988cde4f3aa6f8b
Kris J. Becker <kjbecker@arizona.edu>
2025-01-23 16:36:23 -0700

    Initial commit for C API branch work

---
+++ capi/psmrts_c.h
@@ -0,0 +1,23 @@
+#ifndef psmrts_c_h
+#define psmrts_c_h
+
+
+#ifndef __cplusplus
+# include <stddef.h> /* for size_t definition */
+#else
+#include <cstddef>
+using std::size_t;
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // psmrts_c_h
