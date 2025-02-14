e91fcdf2c0ff289468cce4005698c14f43768c13
Kyle A Becker <kabecker@arizona.edu>
2025-02-14 16:28:45 -0700

    Added value comparative tests for DSK Format / DskKernelModel

--- formats/dsk/tests/test_PsmrtsDSKFormat.cpp
+++ formats/dsk/tests/test_PsmrtsDSKFormat.cpp
@@ -1,9 +1,9 @@
 #include <psmrts_catch2_environment.hpp>
 
 #include <PsmrtsDSKFormat.hpp>
+#include <DskKernelModel.hpp>
 
 TEST_CASE( "DSK FORMAT Asset Test - Default Constructor", "[format][dsk][default]") {
-
     std::string no_file = psmrts_formats_path( "dsk/data/bad_path.bds" );
     psmrts::PsmrtsDSKFormat d_loader;
 
@@ -36,4 +36,62 @@ TEST_CASE( "DSK FORMAT Basic Load/Innit Test", "[format][dsk][shape][bennu]") {
     CHECK( d_loader.get_float_vectors().size()  == 20 );
     CHECK( d_loader.get_double_vectors().size() == 20 );
     CHECK( d_loader.get_indexes().size()        == 36 );
+}
+
+TEST_CASE( "DSK FORMAT Comparative Values Test", "[format][dsk][kernel][bennu]") {
+    const double tolerance = 1.0e-6;
+    std::string dsk_file = psmrts_formats_path( "dsk/data/bennu_20facets.bds" );
+    naif::DskKernelModel dsk( dsk_file );
+    psmrts::PsmrtsDSKFormat d_loader( dsk_file );
+
+    CHECK( dsk.plate_count()    == 36 );
+    CHECK( dsk.n_dsk_segments() == 1);
+
+    psmrts::PsmrtsVector3i model_indexes  = dsk.load_facet_indexes();
+    psmrts::PsmrtsVector3i format_indexes = d_loader.get_indexes();
+
+    for (int i=0; i < d_loader.nIndexes(); i++) {
+        CHECK( format_indexes(i)[0] == model_indexes(i)[0] );
+        CHECK( format_indexes(i)[1] == model_indexes(i)[1] );
+        CHECK( format_indexes(i)[2] == model_indexes(i)[2] );
+    }
+
+    psmrts::PsmrtsVector3d model_vectors  = dsk.load_facet_vectors();
+    psmrts::PsmrtsVector3d format_vectors = d_loader.get_double_vectors();
+
+    for (int i=0; i < d_loader.nVertexes(); i++) {
+        CHECK_THAT( format_vectors(i)[0], Catch::Matchers::WithinAbs( model_vectors(i)[0], tolerance ));
+        CHECK_THAT( format_vectors(i)[1], Catch::Matchers::WithinAbs( model_vectors(i)[1], tolerance ));
+        CHECK_THAT( format_vectors(i)[2], Catch::Matchers::WithinAbs( model_vectors(i)[2], tolerance ));
+    }
+
+    // Conversion Check
+    psmrts::PsmrtsVector3f format_floats = d_loader.get_float_vectors();
+    for (int i=0; i < d_loader.nVertexes(); i++) {
+        CHECK_THAT( format_floats(i)[0], Catch::Matchers::WithinAbs( format_vectors(i)[0], tolerance ));
+        CHECK_THAT( format_floats(i)[1], Catch::Matchers::WithinAbs( format_vectors(i)[1], tolerance ));
+        CHECK_THAT( format_floats(i)[2], Catch::Matchers::WithinAbs( format_vectors(i)[2], tolerance ));
+    }
+
+    psmrts::PsmrtsRayTrace::FacetDatum target_facet;
+    psmrts::PsmrtsRayTrace  raytrace;
+    
+    raytrace.datum().m_hit     = true;
+    raytrace.datum().m_segment = dsk.segment().surfaceid(); 
+    raytrace.datum().m_plateid = 1;
+
+    naif::DskKernelModel::DskIndexDataModel::vector_type ones = naif::DskKernelModel::DskIndexDataModel::vector_type::Ones();
+    for (int i = 0; i < dsk.n_total_plates(); i++) {
+        raytrace.datum().m_plateid = i; 
+        dsk.get_facet( raytrace, target_facet );
+        CHECK ( target_facet.isValid() == true ); 
+        
+        psmrts::PsmrtsRayTrace::FacetDatum format_facet = d_loader.get_mesh().get_facet(i);
+
+        CHECK( target_facet.m_indexes == format_facet.m_indexes );
+        CHECK( target_facet.m_normal  == format_facet.m_normal  );
+        CHECK( target_facet.m_vector1 == format_facet.m_vector1 );
+        CHECK( target_facet.m_vector2 == format_facet.m_vector2 );
+        CHECK( target_facet.m_vector3 == format_facet.m_vector3 );
+    };
 }
