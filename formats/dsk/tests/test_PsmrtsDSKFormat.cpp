6a7af6fefc7e68967ed1bfc34756b166058c2994
Kyle A Becker <kabecker@arizona.edu>
2025-02-21 12:57:30 -0700

    parse_config temporarily suspended, requires Linux-specific ajdustments

--- formats/dsk/tests/test_PsmrtsDSKFormat.cpp
+++ formats/dsk/tests/test_PsmrtsDSKFormat.cpp
@@ -26,7 +26,7 @@ TEST_CASE( "DSK FORMAT Asset Test - Default Constructor", "[format][dsk][default
 TEST_CASE( "DSK FORMAT Basic Load/Innit Test", "[format][dsk][shape][bennu]") {
     std::string dsk_file = psmrts_formats_path( "dsk/data/bennu_20facets.bds" );
     psmrts::PsmrtsDSKFormat d_loader( dsk_file );
-    /**
+    
     CHECK( d_loader.isValid()                   == true );
     CHECK( d_loader.dsk_source()                == psmrts_formats_path( "dsk/data/bennu_20facets.bds" ) );
     CHECK( d_loader.format_model_source()       == psmrts_formats_path( "dsk/data/bennu_20facets.bds" ) );
@@ -37,6 +37,7 @@ TEST_CASE( "DSK FORMAT Basic Load/Innit Test", "[format][dsk][shape][bennu]") {
     CHECK( d_loader.get_double_vectors().size() == 20 );
     CHECK( d_loader.get_indexes().size()        == 36 );
 
+    /**
     nlohmann::ordered_json j_result = nlohmann::ordered_json::object();
 
     j_result["header"]["file"] = d_loader.dsk_source();
@@ -67,7 +68,7 @@ TEST_CASE( "DSK FORMAT Comparative Values Test", "[format][dsk][kernel][bennu]")
     naif::DskKernelModel dsk( dsk_file );
     psmrts::PsmrtsDSKFormat d_loader( dsk_file );
 
-    /** 
+    
     CHECK( dsk.plate_count()    == 36 );
     CHECK( dsk.n_dsk_segments() == 1);
 
@@ -118,5 +119,5 @@ TEST_CASE( "DSK FORMAT Comparative Values Test", "[format][dsk][kernel][bennu]")
         CHECK( target_facet.m_vector2 == format_facet.m_vector2 );
         CHECK( target_facet.m_vector3 == format_facet.m_vector3 );
     };
-    */
+    
 }
