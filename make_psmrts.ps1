292abd3cbb7452d21bc103a4144a415b0a5dfd4c
Kyle Becker <kabecker@Orex.LPL.Arizona.EDU>
2025-03-26 14:24:37 -0700

    Script adjustment, testing

--- make_psmrts.ps1
+++ make_psmrts.ps1
@@ -1,7 +1,59 @@
-#!/usr/bin/env pwsh
+param (
+    [Switch]$t,
+    [Switch]$c,
+    [Switch]$x,
+    [Switch]$d
+)
 
 $env:PSMRTS_ROOT=$PWD
 $env:VCPKG_ROOT="$PWD\vcpkg"
 
+$testopts = ""
+$codecovopts = ""
+$extraopts = ""
+$buildopts = "-DCMAKE_BUILD_TYPE=Release"
+#$usecpus = ""
+
+function Show-Usage {
+    Write-Host "Usage: make_psmrts.ps1 [-t][-c][-x][-d][-j <number of cpus>]"
+    exit 1
+}
+
 .\install_vcpkg.ps1
-.\build_psmrts.ps1 $args
+
+if ($t) {
+    $testopts = "-DBUILD_tESTING=ON"
+}
+
+if ($c) {
+    $codecovopts = "-DEBUILD_COVERAGE=ON"
+    $testopts = "-DEBUILD_TESTING=ON"
+}
+
+if ($x) {
+    $extraopts = "-DEBUILD_EXTRAS=ON"
+}
+
+if ($d) {
+    $buildopts = "-DCMAKE_BUILD_TYPE=Debug"
+}
+
+
+if (-NOT (Test-Path -Path "build")) {
+    New-Item -ItemType Directory -Path "build"
+}
+
+# Above commands are placeholders. 
+# This is min required for testing purposes:
+#cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DCMAKE_TOOLCHAIN-DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -B build -S . $buildopts $testopts $codecovopts $extraopts -DCMAKE_TOOLCHAIN-DCMAKE_TOOLCHAIN_FILE="$PWD\vcpkg\scripts\buildsystems\vcpkg.cmake"
+cmake  --build build
+
+
+#!/usr/bin/env pwsh
+
+#$env:PSMRTS_ROOT=$PWD
+#$env:VCPKG_ROOT="$PWD\vcpkg"
+
+#.\install_vcpkg.ps1
+#.\build_psmrts.ps1 $args
