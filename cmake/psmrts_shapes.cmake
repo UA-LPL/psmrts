489eddfe26c44ae97cd573a631555a1b26096b2a
Kris J. Becker <kjbecker@arizona.edu>
2025-07-21 09:26:52 -0700

    Missed one…

--- cmake/psmrts_shapes.cmake
+++ cmake/psmrts_shapes.cmake
@@ -3,7 +3,7 @@ if(NOT PSMRTS_PROJECT_DIR)
 endif()
 
 set(PSMRTS_INCLUDE_DIRS "${PSMRTS_INCLUDE_DIRS}" 
-                         ${PSMRTS_FORMAT_DIR}/dsk/src
-                         ${PSMRTS_FORMAT_DIR}/obj/src
-                         ${PSMRTS_FORMAT_DIR}/ply/src )
-# message("FormatsIncludes: ${PSMRTS_INCLUDE_DIRS}")
+                         ${PSMRTS_SHAPES_DIR}/dsk/src
+                         ${PSMRTS_SHAPES_DIR}/obj/src
+                         ${PSMRTS_SHAPES_DIR}/ply/src )
+# message("ShapesIncludes: ${PSMRTS_INCLUDE_DIRS}")
