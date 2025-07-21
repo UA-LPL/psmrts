e92111842fc9e505fc4a76b71f7d1a278db462e6
Kris J. Becker <kjbecker@arizona.edu>
2025-07-21 09:34:15 -0700

    Rename PsmrtsShapeTracer to PsmrtsTracer
    Still need to determine of we also change the name of the files for our tracers such as BulletShapeTracer->BulletTracer, etc…

--- psmrts/src/PsmrtsTracer.hpp
+++ psmrts/src/PsmrtsTracer.hpp
@@ -1,5 +1,5 @@
-#ifndef PsmrtsShapeTracer_hpp
-#define PsmrtsShapeTracer_hpp
+#ifndef PsmrtsTracer_hpp
+#define PsmrtsTracer_hpp
 
 #include <exception>
 #include <string>
@@ -20,62 +20,62 @@
 
 namespace psmrts {
 
-  class PsmrtsShapeTracer : public ProductProcessDispatch< MissingProcessRequestHandler, EllipsoidShapeTracer, BulletShapeTracer, NaifDskShapeTracer> {
+  class PsmrtsTracer : public ProductProcessDispatch< MissingProcessRequestHandler, EllipsoidShapeTracer, BulletShapeTracer, NaifDskShapeTracer> {
     public:
-      using PsmrtsTracer = ProductProcessDispatch::ProductType;
+      using Tracer = ProductProcessDispatch::ProductType;
 
-      PsmrtsShapeTracer( )  {  }
-      PsmrtsShapeTracer( const PsmrtsTracer &tracer ) : ProductProcessDispatch( tracer ) {  }
-      virtual ~PsmrtsShapeTracer() { }
+      PsmrtsTracer( )  {  }
+      PsmrtsTracer( const Tracer &tracer ) : ProductProcessDispatch( tracer ) {  }
+      virtual ~PsmrtsTracer() { }
 
-      inline static PsmrtsShapeTracer sphere( const double radius_km, const std::string &name="sphere" ) {
+      inline static PsmrtsTracer sphere( const double radius_km, const std::string &name="sphere" ) {
         Eigen::Vector3d radii( { radius_km, radius_km, radius_km } );
-        return ( PsmrtsShapeTracer( EllipsoidShapeTracer( radii, name ) ) );
+        return ( PsmrtsTracer( EllipsoidShapeTracer( radii, name ) ) );
       }
 
-      inline static PsmrtsShapeTracer spheroid( const double a_km, const double c_km, 
+      inline static PsmrtsTracer spheroid( const double a_km, const double c_km, 
                                                 const std::string &name="spheroid" ) {
         Eigen::Vector3d radii( { a_km, c_km, c_km} );
-        return ( PsmrtsShapeTracer( EllipsoidShapeTracer( radii, name ) ) ); 
+        return ( PsmrtsTracer( EllipsoidShapeTracer( radii, name ) ) ); 
       }
 
-      inline static PsmrtsShapeTracer ellipsoid( const double a_km,  const double b_km, const double c_km, 
+      inline static PsmrtsTracer ellipsoid( const double a_km,  const double b_km, const double c_km, 
                                                 const std::string &name="ellipsoid" ) {
         Eigen::Vector3d radii( { a_km, b_km, c_km } );
-        return ( PsmrtsShapeTracer( EllipsoidShapeTracer( radii, name ) ) ); 
+        return ( PsmrtsTracer( EllipsoidShapeTracer( radii, name ) ) ); 
       }
 
-      inline static PsmrtsShapeTracer ellipsoid( const Eigen::Vector3d radii, 
+      inline static PsmrtsTracer ellipsoid( const Eigen::Vector3d radii, 
                                                 const std::string &name="ellipsoid" ) {
-        return ( PsmrtsShapeTracer( EllipsoidShapeTracer( radii, name ) ) ); 
+        return ( PsmrtsTracer( EllipsoidShapeTracer( radii, name ) ) ); 
       }
 
-      inline static PsmrtsShapeTracer bullet( const std::string &meshfile ) {
+      inline static PsmrtsTracer bullet( const std::string &meshfile ) {
 
         std::string fext_t = psmrts_tolower( psmrts_file_extension( meshfile ) );
         if ( "obj" == fext_t ) {
-          return ( PsmrtsShapeTracer( BulletShapeTracer( psmrts::bullet::PsmrtsBulletWorldModel( psmrts::bullet::PsmrtsBulletMeshMap ( psmrts::PsmrtsOBJFormat( meshfile ) ), meshfile ) ) ) );
+          return ( PsmrtsTracer( BulletShapeTracer( psmrts::bullet::PsmrtsBulletWorldModel( psmrts::bullet::PsmrtsBulletMeshMap ( psmrts::PsmrtsOBJFormat( meshfile ) ), meshfile ) ) ) );
         }
         else if ( "ply" == fext_t ) {
           PsmrtsMeshData mesh_t(  PsmrtsPLYFormat( meshfile ).get_mesh() );
           psmrts::bullet::PsmrtsBulletMeshMap mesh_b( mesh_t, meshfile, 0 );
           psmrts::bullet::PsmrtsBulletWorldModel bt_world( mesh_b, meshfile );
-          return ( PsmrtsShapeTracer( BulletShapeTracer( bt_world ) ) );
+          return ( PsmrtsTracer( BulletShapeTracer( bt_world ) ) );
         }
         else {  // ( "dsk" == fext_t )
           naif::DskKernelModel dsk( meshfile );
           PsmrtsMeshData mesh_t( dsk.load_facet_indexes(), dsk.load_facet_vectors() );
           psmrts::bullet::PsmrtsBulletMeshMap mesh_b( mesh_t, meshfile, 0 );
           psmrts::bullet::PsmrtsBulletWorldModel bt_world( mesh_b, meshfile );
-          return ( PsmrtsShapeTracer( BulletShapeTracer( bt_world ) ) );           
+          return ( PsmrtsTracer( BulletShapeTracer( bt_world ) ) );           
         }
 
         // Likely won't reach here...
-        return ( PsmrtsShapeTracer( MissingProcessRequestHandler( "PsmrtsShapeTracer" ) ) );
+        return ( PsmrtsTracer( MissingProcessRequestHandler( "PsmrtsTracer" ) ) );
       }
 
-      inline static PsmrtsShapeTracer naifdsk( const std::string &dskfile ) {
-        return ( PsmrtsShapeTracer( NaifDskShapeTracer( dskfile ) ) ); 
+      inline static PsmrtsTracer naifdsk( const std::string &dskfile ) {
+        return ( PsmrtsTracer( NaifDskShapeTracer( dskfile ) ) ); 
       }
 
     };
