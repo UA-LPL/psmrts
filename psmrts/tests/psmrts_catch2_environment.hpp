748400f016a91b279ebad22a4e5b4322cce08810
acpaquette <acpaquette@usgs.gov>
2025-07-24 16:22:27 -0700

    Added generater include to test harness

--- psmrts/tests/psmrts_catch2_environment.hpp
+++ psmrts/tests/psmrts_catch2_environment.hpp
@@ -6,6 +6,7 @@
 // include the Catch framework
 #include <catch2/catch_version.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include <psmrts_version.h>
