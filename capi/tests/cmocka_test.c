25b948b9e79980073ea7d63738508d3219a329f6
Kyle A Becker <kabecker@arizona.edu>
2025-08-04 13:20:59 -0700

    Adjusted cmocka tests to double tolerance, various code fixes, and tests for conversion functions in cmocka_test.c

--- capi/tests/cmocka_test.c
+++ capi/tests/cmocka_test.c
@@ -1,9 +1,12 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
+#include <stdio.h>
 #include <cmocka.h>
 
 #include "psmrts_c.h" 
+
+const double tolerance = 1e-12;
 
 // --- PSMRTS information Functions ---
 static void test_psmrts_version_info(void **state) {
@@ -22,9 +25,9 @@ static void test_psmrts_vector3d_init(void **state) {
 
     PSMRTS_Vector3d v = psmrts_vector3d(1.0, 2.0, 3.0);
 
-    assert_float_equal(v.x, 1.0, 1e-6);
-    assert_float_equal(v.y, 2.0, 1e-6);
-    assert_float_equal(v.z, 3.0, 1e-6);
+    assert_double_equal(v.x, 1.0, tolerance);
+    assert_double_equal(v.y, 2.0, tolerance);
+    assert_double_equal(v.z, 3.0, tolerance);
 }
 
 static void test_psmrts_negate(void **state) {
@@ -33,16 +36,16 @@ static void test_psmrts_negate(void **state) {
     PSMRTS_Vector3d v = psmrts_vector3d(2.0, 3.0, 4.0);
     PSMRTS_Vector3d v_neg = psmrts_negate( &v );
 
-    assert_float_equal(v_neg.x, -2.0, 1e-6);
-    assert_float_equal(v_neg.y, -3.0, 1e-6);
-    assert_float_equal(v_neg.z, -4.0, 1e-6);
+    assert_double_equal(v_neg.x, -2.0, tolerance);
+    assert_double_equal(v_neg.y, -3.0, tolerance);
+    assert_double_equal(v_neg.z, -4.0, tolerance);
 
     PSMRTS_Vector3d v2 = psmrts_vector3d(2.0, -3.0, 4.0);
     PSMRTS_Vector3d v2_neg = psmrts_negate( &v2 );
 
-    assert_float_equal(v2_neg.x, -2.0, 1e-6);
-    assert_float_equal(v2_neg.y, 3.0, 1e-6);
-    assert_float_equal(v2_neg.z, -4.0, 1e-6);
+    assert_double_equal(v2_neg.x, -2.0, tolerance);
+    assert_double_equal(v2_neg.y, 3.0, tolerance);
+    assert_double_equal(v2_neg.z, -4.0, tolerance);
 }
 
 static void test_psmrts_subtract(void **state) {
@@ -52,17 +55,17 @@ static void test_psmrts_subtract(void **state) {
     PSMRTS_Vector3d v2 = psmrts_vector3d(1.0, 2.0, 3.0);
     PSMRTS_Vector3d result = psmrts_subtract(&v1, &v2);
 
-    assert_float_equal(result.x, 3.0, 1e-6);
-    assert_float_equal(result.y, 3.0, 1e-6);
-    assert_float_equal(result.z, 3.0, 1e-6);
+    assert_double_equal(result.x, 3.0, tolerance);
+    assert_double_equal(result.y, 3.0, tolerance);
+    assert_double_equal(result.z, 3.0, tolerance);
 
     PSMRTS_Vector3d v3 = psmrts_vector3d(0.0, 10.0, -4.0);
     PSMRTS_Vector3d v4 = psmrts_vector3d(4.0, 5.0, 6.0);
     PSMRTS_Vector3d result2 = psmrts_subtract(&v3, &v4);
 
-    assert_float_equal(result2.x, -4.0, 1e-6);
-    assert_float_equal(result2.y, 5.0, 1e-6);
-    assert_float_equal(result2.z, -10.0, 1e-6);
+    assert_double_equal(result2.x, -4.0, tolerance);
+    assert_double_equal(result2.y, 5.0, tolerance);
+    assert_double_equal(result2.z, -10.0, tolerance);
 }
 
 static void test_psmrts_add(void **state) {
@@ -72,9 +75,9 @@ static void test_psmrts_add(void **state) {
     PSMRTS_Vector3d v2 = psmrts_vector3d(4.0, 5.0, 6.0);
     PSMRTS_Vector3d result = psmrts_add(&v1, &v2);
 
-    assert_float_equal(result.x, 5.0, 1e-6);
-    assert_float_equal(result.y, 7.0, 1e-6);
-    assert_float_equal(result.z, 9.0, 1e-6);
+    assert_double_equal(result.x, 5.0, tolerance);
+    assert_double_equal(result.y, 7.0, tolerance);
+    assert_double_equal(result.z, 9.0, tolerance);
 }
 
 static void test_psmrts_scale(void **state) {
@@ -83,9 +86,9 @@ static void test_psmrts_scale(void **state) {
     PSMRTS_Vector3d v = psmrts_vector3d(1.0, 2.0, 3.0);
     PSMRTS_Vector3d result = psmrts_scale(&v, 2);
 
-    assert_float_equal(result.x, 2.0, 1e-6);
-    assert_float_equal(result.y, 4.0, 1e-6);
-    assert_float_equal(result.z, 6.0, 1e-6);
+    assert_double_equal(result.x, 2.0, tolerance);
+    assert_double_equal(result.y, 4.0, tolerance);
+    assert_double_equal(result.z, 6.0, tolerance);
 }
 
 static void test_psmrts_length(void **state) {
@@ -94,7 +97,7 @@ static void test_psmrts_length(void **state) {
     PSMRTS_Vector3d v = psmrts_vector3d(1.0, 4.0, 8.0);
     double result = psmrts_length(&v);
 
-    assert_float_equal(result, 9.0, 1e-6);
+    assert_double_equal(result, 9.0, tolerance);
 }
 
 // --- PSMRTS Trace Functions ---
@@ -111,25 +114,22 @@ static void test_psmrts_ray(void **state) {
     PSMRTS_Vector3d obs_result = psmrts_ray_observer(ray);
     PSMRTS_Vector3d lkdr_result = psmrts_ray_lookdir(ray);
 
-    assert_float_equal(obs_result.x, 2.0, 1e-6);
-    assert_float_equal(obs_result.y, 4.0, 1e-6);
-    assert_float_equal(obs_result.z, 6.0, 1e-6);
+    assert_double_equal(obs_result.x, 2.0, tolerance);
+    assert_double_equal(obs_result.y, 4.0, tolerance);
+    assert_double_equal(obs_result.z, 6.0, tolerance);
 
-    assert_float_equal(lkdr_result.x, -1.0, 1e-6);
-    assert_float_equal(lkdr_result.y, 0.0, 1e-6);
-    assert_float_equal(lkdr_result.z, 12.0, 1e-6);
+    assert_double_equal(lkdr_result.x, -1.0, tolerance);
+    assert_double_equal(lkdr_result.y, 0.0, tolerance);
+    assert_double_equal(lkdr_result.z, 12.0, tolerance);
 
     PSMRTS_BOOL nohit = psmrts_ray_has_hit(ray);
 
     assert_int_equal(nohit, 0);
-
-    /*
+    
     PSMRTS_Vector3d raypt = psmrts_ray_raypt(ray);
-    assert_int_equal(raypt.x, 0.0);
-    assert_int_equal(raypt.y, 0.0);
-    assert_int_equal(raypt.z, 0.0);
-    // checked just to see, numbers result in hexadec #s
-    */
+    assert_double_equal(raypt.x, -2.0, tolerance);
+    assert_double_equal(raypt.y, -4.0, tolerance);
+    assert_double_equal(raypt.z, -6.0, tolerance);    
 
     PSMRTS_Vector3d new_obs = psmrts_vector3d(3.0, -5.0, 7.0);
     PSMRTS_Vector3d new_lkdr = psmrts_vector3d(100.0, 20.2, 30.0);
@@ -139,13 +139,13 @@ static void test_psmrts_ray(void **state) {
     obs_result = psmrts_ray_observer(ray);
     lkdr_result = psmrts_ray_lookdir(ray);
 
-    assert_float_equal(obs_result.x, 3.0, 1e-6);
-    assert_float_equal(obs_result.y, -5.0, 1e-6);
-    assert_float_equal(obs_result.z, 7.0, 1e-6);
+    assert_double_equal(obs_result.x, 3.0, tolerance);
+    assert_double_equal(obs_result.y, -5.0, tolerance);
+    assert_double_equal(obs_result.z, 7.0, tolerance);
 
-    assert_float_equal(lkdr_result.x, 100.0, 1e-6);
-    assert_float_equal(lkdr_result.y, 20.2, 1e-6);
-    assert_float_equal(lkdr_result.z, 30.0, 1e-6);
+    assert_double_equal(lkdr_result.x, 100.0, tolerance);
+    assert_double_equal(lkdr_result.y, 20.2, tolerance);
+    assert_double_equal(lkdr_result.z, 30.0, tolerance);
 }
 
 static void test_psmrts_raytrace(void **state) {
@@ -154,7 +154,7 @@ static void test_psmrts_raytrace(void **state) {
     PSMRTS_Tracer *ellipse = psmrts_create_sphere(1.0, name);
 
     PSMRTS_Vector3d obs = psmrts_vector3d(0.0, 0.0, 3.0);
-    PSMRTS_Vector3d lkdr = psmrts_vector3d(0.0, 0.0, -1.0);
+    PSMRTS_Vector3d lkdr = psmrts_vector3d(0.0, -0.2, -1.0);
     PSMRTS_RayTrace *ray = psmrts_create_ray(&obs, &lkdr);
 
     PSMRTS_RayTrace *raytrace = psmrts_ray_trace(ray, ellipse);
@@ -163,29 +163,37 @@ static void test_psmrts_raytrace(void **state) {
     assert_int_equal(hashit, 1);
 
     PSMRTS_Vector3d xyz = psmrts_ray_xyz(raytrace);
-    assert_float_equal(xyz.x, 0.0, 1e-6);
-    assert_float_equal(xyz.y, 0.0, 1e-6);
-    assert_float_equal(xyz.z, 1.0, 1e-6);
+    assert_double_equal(xyz.x, 0.0, tolerance);
+    assert_double_equal(xyz.y, -0.418342091322, tolerance);
+    assert_double_equal(xyz.z, 0.908289543388, tolerance);
 
     PSMRTS_Vector3d raypt = psmrts_ray_raypt(raytrace);
-    assert_float_equal(raypt.x, 0.0, 1e-6);
-    assert_float_equal(raypt.y, 0.0, 1e-6);
-    assert_float_equal(raypt.z, -2.0, 1e-6);
+    assert_double_equal(raypt.x, 0.0, tolerance);
+    assert_double_equal(raypt.y, -0.418342091322, tolerance);
+    assert_double_equal(raypt.z, -2.091710456612, tolerance);
 
     PSMRTS_Vector3d normal = psmrts_ray_normal(raytrace);
-    assert_float_equal(normal.x, 0.0, 1e-6);
-    assert_float_equal(normal.y, 0.0, 1e-6);
-    assert_float_equal(normal.z, 1.0, 1e-6);
+    /** 
+    printf("normal.x: %.12f, y: %.12f, z: %.12f\n", normal.x, normal.y, normal.z);
+    fflush(stdout);
+    */
+    assert_double_equal(normal.x, 0.0, tolerance);
+    assert_double_equal(normal.y, -0.418342091322, tolerance);
+    assert_double_equal(normal.z, 0.908289543388, tolerance);
 
     double intercept = psmrts_ray_intercept_radius(raytrace);
-    assert_float_equal(intercept, 1.0, 1e-6);
+    assert_double_equal(intercept, 1.0, tolerance);
 
     double slant = psmrts_ray_intercept_slant_distance(raytrace);
-    assert_float_equal(slant, 2.0, 1e-6);
+    /*
+    printf("slant: %.12f\n", slant);
+    fflush(stdout);
+    */
+    assert_double_equal(slant, 2.133134487010, tolerance);
 
     // Second Ray Trace
     PSMRTS_Vector3d obs2 = psmrts_vector3d(0.0, 2.0, 4.0);
-    PSMRTS_Vector3d lkdr2 = psmrts_vector3d(0.0, -2.0, -2.0);
+    PSMRTS_Vector3d lkdr2 = psmrts_vector3d(-0.2, -2.0, -2.0);
     PSMRTS_RayTrace *ray2 = psmrts_create_ray(&obs, &lkdr);
 
     PSMRTS_RayTrace *raytrace2 = psmrts_ray_trace(ray2, ellipse);
@@ -193,24 +201,26 @@ static void test_psmrts_raytrace(void **state) {
     assert_int_equal(hashit2, 1); 
 
     double dist = psmrts_ray2ray_distance(raytrace, raytrace2);
-    assert_float_equal(dist, 0.0, 1.e-6); // Should this be 0?
+    assert_double_equal(dist, 0.0, tolerance); // Should this be 0?
 
     PSMRTS_Vector3d v1 = psmrts_vector3d(1.0, 0.0, 0.0);
     PSMRTS_Vector3d v2 = psmrts_vector3d(1.0, 1.0, 0.0); 
     double sep_angle = psmrts_separation_angle_radians(&v1, &v2);
-    assert_float_equal(sep_angle, 0.785398, 1e-6);
+    //printf("sep_angle: %.12f\n", sep_angle);
+    //fflush(stdout);
+    assert_double_equal(sep_angle, 0.785398163397, tolerance);
 
     PSMRTS_BOOL near = psmrts_isNear(raytrace, raytrace2, 5.0);
     assert_int_equal(near, 1);
 
     double incidence = psmrts_incidence(raytrace, raytrace2);
-    assert_float_equal(incidence, 0.0, 1e-6);
+    assert_double_equal(incidence, 0.629014802427, tolerance);
 
     double emission = psmrts_emission(raytrace);
-    assert_float_equal(emission, 0.0, 1e-6);
+    assert_double_equal(emission, 0.629014802427, tolerance);
 
     double phase = psmrts_phase(raytrace, raytrace2);
-    assert_float_equal(phase, 0.0, 1e-6);
+    assert_double_equal(phase, 0.0, tolerance);
 }
 
 // --- PSMRTS TraceArray Functions ---
@@ -246,15 +256,15 @@ static void test_psmrts_trace_array(void **state) {
 
     const PSMRTS_RayTrace *first = psmrts_trace_array_get_trace(t_array, 0);
     PSMRTS_Vector3d first_obs = psmrts_ray_observer(first);
-    assert_float_equal(first_obs.x, 0.0, 1e-6);
-    assert_float_equal(first_obs.y, 0.0, 1e-6);
-    assert_float_equal(first_obs.z, 3.0, 1e-6);
+    assert_double_equal(first_obs.x, 0.0, tolerance);
+    assert_double_equal(first_obs.y, 0.0, tolerance);
+    assert_double_equal(first_obs.z, 3.0, tolerance);
 
     const PSMRTS_RayTrace *second = psmrts_trace_array_get_trace(t_array, 1);
     PSMRTS_Vector3d sec_obs = psmrts_ray_observer(second);
-    assert_float_equal(sec_obs.x, 0.0, 1e-6);
-    assert_float_equal(sec_obs.y, 3.0, 1e-6);
-    assert_float_equal(sec_obs.z, 2.0, 1e-6);
+    assert_double_equal(sec_obs.x, 0.0, tolerance);
+    assert_double_equal(sec_obs.y, 3.0, tolerance);
+    assert_double_equal(sec_obs.z, 2.0, tolerance);
 }
 
 // --- PSMRTS Photometric Trace/Array Functions ---
@@ -282,28 +292,28 @@ static void test_psmrts_photometric_trace(void **state) {
 
     /** Need photometric specific ray trace to test further
     double incidence = psmrts_photometric_incidence(p_ray);
-    assert_float_equal(incidence, 0.0, 1e-6);
+    assert_double_equal(incidence, 0.0, 1e-6);
 
     double emission = psmrts_photometric_emission(p_ray);
-    assert_float_equal(emission, 0.0, 1e-6);
+    assert_double_equal(emission, 0.0, 1e-6);
 
     double phase = psmrts_photometric_phase(p_ray);
-    assert_float_equal(phase, 0.0, 1e-6);
+    assert_double_equal(phase, 0.0, 1e-6);
     */
 
     const PSMRTS_RayTrace *obs_ray = psmrts_photometric_observer_trace(p_ray);
     PSMRTS_Vector3d obs_result = psmrts_ray_observer(obs_ray);
-    assert_float_equal(obs_result.x, obs.x, 1e-6);
-    assert_float_equal(obs_result.y, obs.y, 1e-6);
-    assert_float_equal(obs_result.z, obs.z, 1e-6);
+    assert_double_equal(obs_result.x, obs.x, tolerance);
+    assert_double_equal(obs_result.y, obs.y, tolerance);
+    assert_double_equal(obs_result.z, obs.z, tolerance);
 
     // For this, would the sun be the observer? How can we retrieve the
     // sunpos from this structure..?
     const PSMRTS_RayTrace *sun_ray = psmrts_photometric_sun_trace(p_ray);
     //PSMRTS_Vector3d sun_result = psmrts_ray_observer(obs_ray);
-    //assert_float_equal(sun_result.x, sun_pos.x, 1e-6);
-    //assert_float_equal(sun_result.y, sun_pos.y, 1e-6);
-    //assert_float_equal(sun_result.z, sun_pos.z, 1e-6);
+    //assert_double_equal(sun_result.x, sun_pos.x, 1e-6);
+    //assert_double_equal(sun_result.y, sun_pos.y, 1e-6);
+    //assert_double_equal(sun_result.z, sun_pos.z, 1e-6);
 
     PSMRTS_Vector3d new_obs = psmrts_vector3d(2.0, 4.0, 6.0);
     PSMRTS_Vector3d new_lkdr = psmrts_negate(&new_obs);
@@ -311,9 +321,9 @@ static void test_psmrts_photometric_trace(void **state) {
     psmrts_photometric_ray_set_observation(&new_obs, &new_lkdr, &sun_pos, p_ray);
     const PSMRTS_RayTrace *new_obs_ray = psmrts_photometric_observer_trace(p_ray);
     PSMRTS_Vector3d new_obs_result = psmrts_ray_observer(new_obs_ray);
-    assert_float_equal(new_obs_result.x, new_obs.x, 1e-6);
-    assert_float_equal(new_obs_result.y, new_obs.y, 1e-6);
-    assert_float_equal(new_obs_result.z, new_obs.z, 1e-6);
+    assert_double_equal(new_obs_result.x, new_obs.x, tolerance);
+    assert_double_equal(new_obs_result.y, new_obs.y, tolerance);
+    assert_double_equal(new_obs_result.z, new_obs.z, tolerance);
 }
 
 static void test_psmrts_photometric_array(void **state) {
@@ -347,19 +357,58 @@ static void test_psmrts_photometric_array(void **state) {
     size = psmrts_photometric_trace_array_size(p_array);
     assert_int_equal(size, 2);
 
-    // May need to consider obs, look dir, sun getters for photometric ray traces
-    // Also, should I need to be casting const values here?
+    // Photometric Trace Array
     /** 
-    PSMRTS_PhotometricRayTrace *target = psmrts_photometric_trace_array_get_trace(p_array, 1);
-    PSMRTS_RayTrace *target_ray = psmrts_photometric_observer_trace(target);
+    const PSMRTS_PhotometricRayTrace *target = psmrts_photometric_trace_array_get_trace(p_array, 1);
+    const PSMRTS_RayTrace *target_ray = psmrts_photometric_observer_trace(target);
     PSMRTS_Vector3d target_obs = psmrts_ray_observer(target_ray);
-    assert_float_equal(target_obs.x, obs2.x, 1e-6);
-    assert_float_equal(target_obs.y, obs2.y, 1e-6);
-    assert_float_equal(target_obs.z, obs2.z, 1e-6);
+    assert_double_equal(target_obs.x, obs2.x, 1e-6);
+    assert_double_equal(target_obs.y, obs2.y, 1e-6);
+    assert_double_equal(target_obs.z, obs2.z, 1e-6);
     */
 }
 
+// --- Conversion Fuctions ---
+static void test_psmrts_conversions(void **state) {
+    (void)state;
+
+    // 50 degrees | 20 degrees | 100 radius
+    PSMRTS_Vector3d lonlat = psmrts_vector3d(50.0, 20.0, 100); 
+
+    PSMRTS_Vector3d xyz_result = psmrts_lonlatrad_to_xyz_d(&lonlat);
+    assert_double_equal(xyz_result.x, 60.402277355505, tolerance);
+    assert_double_equal(xyz_result.y, 71.984631039295, tolerance);
+    assert_double_equal(xyz_result.z, 34.202014332567, tolerance);
+
+    PSMRTS_Vector3d reverse = psmrts_xyz_to_lonlatrad_d(&xyz_result);
+    assert_double_equal(reverse.x, lonlat.x, tolerance);
+    assert_double_equal(reverse.y, lonlat.y, tolerance);
+    assert_double_equal(reverse.z, lonlat.z, tolerance);
+
+    double degree = 30;
+    double deg2rad = psmrts_degrees_to_radians(degree);
+    assert_double_equal(deg2rad, 0.523598775598, tolerance);
+
+    double rad2deg = psmrts_radians_to_degrees(deg2rad);
+    assert_double_equal(rad2deg, degree, tolerance);
+
+    PSMRTS_Vector3d deg_vec = psmrts_vector3d(30, 75, 10.0);
+
+    // Note: only converts first two values, 3rd is radius
+    PSMRTS_Vector3d rad_vec = psmrts_vector3d_to_radians(&deg_vec);
+    assert_double_equal(rad_vec.x, 0.523598775598, tolerance);
+    assert_double_equal(rad_vec.y, 1.308996938996, tolerance);
+    assert_double_equal(rad_vec.z, 10.0, tolerance);
+
+    PSMRTS_Vector3d rev_vec = psmrts_vector3d_to_degrees(&rad_vec);
+    assert_double_equal(rev_vec.x, deg_vec.x, tolerance);
+    assert_double_equal(rev_vec.y, deg_vec.y, tolerance);
+    assert_double_equal(rev_vec.z, deg_vec.z, tolerance);
+}
+
 int main(void) {
+    cmocka_set_message_output(CM_OUTPUT_STDOUT);
+
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_psmrts_version_info),
         cmocka_unit_test(test_psmrts_vector3d_init),
@@ -373,6 +422,7 @@ int main(void) {
         cmocka_unit_test(test_psmrts_trace_array),
         cmocka_unit_test(test_psmrts_photometric_trace),
         cmocka_unit_test(test_psmrts_photometric_array),
+        cmocka_unit_test(test_psmrts_conversions),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
