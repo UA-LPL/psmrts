add35697f09e71f60c5aeac2022ad2ea99951080
Kris J. Becker <kjbecker@arizona.edu>
2025-08-15 07:34:25 -0700

    Standardize on CMAKE BUILD_TESTING
    The CMAKE standard for enabling testing project wide is BUILD_TESTING (https://cmake.org/cmake/help/latest/variable/BUILD_TESTING.html). This commit aligns our usage with this standard.

--- scripts/build_psmrts.sh
+++ scripts/build_psmrts.sh
@@ -11,11 +11,11 @@ usecpus=""
 while getopts ":htcdxj:" o; do
     case "${o}" in
         t)
-            testopts="-DBUILD_TESTS=ON"
+            testopts="-DBUILD_TESTING=ON"
             ;;
         c)
             codecovopts="-DBUILD_COVERAGE=ON"
-            testopts="-DBUILD_TESTS=ON"
+            testopts="-DBUILD_TESTING=ON"
 
             ;;
         x)
