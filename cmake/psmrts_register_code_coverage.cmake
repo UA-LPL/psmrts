771401e1b5b78d7ab2f871a9f6de0f842b692150
Kris J. Becker <kjbecker@arizona.edu>
2025-08-18 11:10:40 -0700

    Rename psmrts_log_code_coverage
    Took the opportunity to chnage the name of the CMAKE function psmrts_log_code_coverage to psmrts_register_code_coverage. All CMakeLists.txt files in the tests directories have been updated. It seems to better represent what is purpose is.

--- cmake/psmrts_register_code_coverage.cmake
+++ cmake/psmrts_register_code_coverage.cmake
@@ -2,13 +2,13 @@
 ### Add a test application and any exclusions to participate in code coverage
 
 __NOTE__ this function does not call add_test() or any testing framework
-test discovery function. That is the responsibility of the developer.
+test discovery functions. That is the responsibility of the developer.
 This function manages the parameters that are provided to the
 setup_target_for_coverage_gcovr_html() function.
 
 
 ```
-psmrts_log_code_coverage(
+psmrts_register_code_coverage(
       DEPENDENCY testapp
       EXCLUDES   "paths/*;files"
 )
@@ -23,9 +23,10 @@ The `PSMRTS_COVERAGE_DEPENDENCIES` parameter is accumulates the `DEPENDENCIES`
 parameter. This will typically be your Catch2/GTest application name provided
 in add_test(). This is an optional parameter.
 
-The `PSMRTS_COVERAGE_EXCLUDES` parameter accumulates a list of files to exclude
-from code coverage. File paths are typically relative to the CURRENT_SOURCE_DIR
-and will contain a glob, e.g., "relpath/*;excluded_file.cpp".
+The `PSMRTS_COVERAGE_EXCLUDES` parameter accumulates a list of files given to
+the EXCLUDE parameter from code coverage. File paths are typically relative to
+the root dir. For PSMRTS the gcovr root directory is
+"${PSMRTS_SOURCE_DIR}/psmrts".
 
 The PSMRTS top level determines the BASE_DIRECTORY.
 
@@ -38,16 +39,17 @@ files to exclude (PSMRTS_COVERAGE_EXCLUDES) during the code coverage build.
 Here are some examples of calls for this functions.
 
 ```
-psmrts_log_code_coverage(DEPENDENCY "psmrts_test_suite" EXCLUDES "psmrts_test_suite;paths/*")
-psmrts_log_code_coverage(DEPENDENCY "psmrts_test_suite2")
-psmrts_log_code_coverage(EXCLUDES   "all_the_bad_ones.*")
+psmrts_register_code_coverage(DEPENDENCY "psmrts_test_suite" EXCLUDES "psmrts_test_suite;paths/*")
+psmrts_register_code_coverage(DEPENDENCY "psmrts_test_suite2")
+psmrts_register_code_coverage(EXCLUDES   "all_the_bad_ones.*")
+psmrts_register_code_coverage(EXCLUDES   "core/tests/psmrts_catch2_environment.hpp")  
 ```
 #]==]
-function(psmrts_log_code_coverage)
+function(psmrts_register_code_coverage)
 
     include(CMakeParseArguments)
     cmake_parse_arguments(
-        psmrts_log
+        psmrts_register
         ""
         ""
         "DEPENDENCY;EXCLUDES"
@@ -55,19 +57,19 @@ function(psmrts_log_code_coverage)
     )
 
     # Check on the call status
-    if (psmrts_log_UNPARSED_ARGUMENTS)
+    if (psmrts_register_UNPARSED_ARGUMENTS)
       message(FATAL_ERROR
-        "There are unparsed arguments to psmrts_log_code_coverage(): "
-        "${psmrts_log_UNPARSED_ARGUMENTS}"
+        "There are unparsed arguments to psmrts_register_code_coverage(): "
+        "${psmrts_register_UNPARSED_ARGUMENTS}"
       )
-    endif (psmrts_log_UNPARSED_ARGUMENTS)
+    endif (psmrts_register_UNPARSED_ARGUMENTS)
 
-    if(NOT "${psmrts_log_DEPENDENCY}" STREQUAL "")
-      set_property(GLOBAL APPEND PROPERTY PSMRTS_COVERAGE_DEPENDENCIES "${psmrts_log_DEPENDENCY}" )
+    if(NOT "${psmrts_register_DEPENDENCY}" STREQUAL "")
+      set_property(GLOBAL APPEND PROPERTY PSMRTS_COVERAGE_DEPENDENCIES "${psmrts_register_DEPENDENCY}" )
     endif()
 
-    if(NOT "${psmrts_log_EXCLUDES}" STREQUAL "")
-      set_property(GLOBAL APPEND PROPERTY PSMRTS_COVERAGE_EXCLUDES "${psmrts_log_EXCLUDES}" )
+    if(NOT "${psmrts_register_EXCLUDES}" STREQUAL "")
+      set_property(GLOBAL APPEND PROPERTY PSMRTS_COVERAGE_EXCLUDES "${psmrts_register_EXCLUDES}" )
     endif()
 
-endfunction(psmrts_log_code_coverage)
+endfunction(psmrts_register_code_coverage)
