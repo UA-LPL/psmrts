d2b843e8115e5a2c68a4b965a53616c382bf85d1
Kris J. Becker <kjbecker@arizona.edu>
2025-08-29 11:57:08 -0700

    Add initial implementation of ProductConfiguration

--- psmrts/core/ProductConfiguration.hpp
+++ psmrts/core/ProductConfiguration.hpp
@@ -2,12 +2,14 @@
 #ifndef ProductConfiguration_hpp
 #define ProductConfiguration_hpp
 
-#include <memory>
 #include <string>
-#include <vector>
 #include <exception>
+#include <memory>
+#include <vector>
+#include <initializer_list>
 
-#include <psmrts/core/ProductFeature.hpp>
+#include <psmrts/core/PsmrtsUtilities.hpp>
+#include <psmrts/core/ProductOption.hpp>
 
 namespace psmrts { 
 
@@ -23,47 +25,77 @@ namespace psmrts {
    */
   class ProductConfiguration {
     public:
-      using ProductFeatureList = std::vector<ProductFeature>;
+      using ProductOptionList = std::vector<ProductOption>;
 
-      ProductConfiguration( ) : m_request_j(), m_parameters{} { }
-
-      /** 
-      ProductConfiguration( const ordered_json &request ) {
-        m_request_j = request;
-        (void) parse_parameters( request, m_parameters );
-      }
-      */
-
-      ProductConfiguration( const std::vector<ProductFeature> &parameters ) :
-                      m_request_j(), m_parameters{} { 
-        // Unbind them all to the JSON struct...
-
-      }
+      ProductConfiguration( ) : m_name("none"), m_options{} { }
+      ProductConfiguration(const std::string &name ) : m_name(), m_options{} { }
+      explicit ProductConfiguration( const std::string &name, 
+                                     const std::initializer_list<ProductOption> &options ) : 
+                                     m_name( psmrts_tolower(name) ),
+                                     m_options( options.begin(), options.end() ) { }                                
+      explicit ProductConfiguration( const std::string &name, 
+                                     const std::vector<ProductOption> &options ) : 
+                                     m_name( psmrts_tolower(name) ),
+                                     m_options( options ) { }      
 
       virtual ~ProductConfiguration() = default;
 
 
+      inline const std::string &name() const {
+        return ( m_name );
+      }
+
       inline size_t size() const {
-        return ( m_parameters.size() );
+        return ( m_options.size() );
       }
 
-      /**
-      inline void add_parameter( const ProductFeature &parm ) { 
-
+      inline bool contains( const std::string &name ) const {
+        std::string name_l = psmrts_tolower( name );
+        for ( auto const &option : m_options ) {
+          if ( option.name() == name_l ) {
+            return ( true );
+          }
+        }
+        return ( false );
       }
 
-      inline ProductFeature &get_parameter( const std::string &parm ) {       
-      
+      inline void add( const ProductOption &option ) {
+        m_options.push_back( option );
+        return;
       }
 
-      inline int parse_parameters( const ordered_json, ProductFeatureList &plist ) {
-
+      inline const ProductOption &find( const std::string &name ) {       
+        std::string name_l = psmrts_tolower( name );
+        for ( auto const &option : m_options ) {
+          if ( option.name() == name_l ) {
+            return ( option );
+          }
+        }  
+        throw std::runtime_error( "*** ProductOption::get(" +  name +  " ) error - option not found" );
       }
-      */
+
+      /** Remove the specifed option */
+      inline bool remove( const std::string &name ) {   
+        std::string name_l = psmrts_tolower( name );
+        ProductOptionList::iterator opt_t = m_options.begin();
+
+        while ( opt_t != m_options.end() ) {
+          if ( opt_t->name() == name_l ) {
+            m_options.erase( opt_t );
+            return ( true );
+          }
+          ++opt_t;
+        }  
+        return ( false );
+      }
+
+      inline void clear() {
+        m_options.clear();
+      }
+
     protected:
-      ordered_json         m_request_j;
-      ProductFeatureList   m_parameters;
-
+      std::string       m_name;
+      ProductOptionList m_options;
   };
 
 
