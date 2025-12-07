c35c4d7a8049576432fd93745ddc9e74ead4bdf4
Kris J. Becker <kjbecker@arizona.edu>
2025-12-06 22:36:54 -0700

    Set rescan directly in Bullet target

--- cmake/psmrts_dependency_standards.cmake
+++ cmake/psmrts_dependency_standards.cmake
@@ -111,8 +111,9 @@ macro(psmrts_add_bullet_target)
           INTERFACE_COMPILE_DEFINITIONS "${BULLET_DEFINITIONS}"
           INTERFACE_INCLUDE_DIRECTORIES "${BULLET_INCLUDE_DIR}"
           INTERFACE_LINK_DIRECTORIES "${BULLET_LIBRARY_DIRS}"
-          INTERFACE_LINK_LIBRARIES "${BULLET_LIBRARIES}"
+          INTERFACE_LINK_LIBRARIES_DIRECT "${BULLET_LIBRARIES}"
       )
+
       set(Bullet_double_FOUND TRUE)
       message(STATUS "Bullet Double Target Created/Confirmed: Bullet::Bullet_double")
       message(STATUS "Bullet Includes:  ${BULLET_INCLUDE_DIR}")
