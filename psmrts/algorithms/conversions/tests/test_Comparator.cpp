34b1677370e29fcca013d483063d5ec3aaefa9a7
kledmundson <k.l.edmundson@gmail.com>
2026-01-27 09:14:23 -0700

    Preliminary version of test_Comparator.cpp.

---
+++ psmrts/algorithms/conversions/tests/test_Comparator.cpp
@@ -0,0 +1,67 @@
+#include <psmrts/core/tests/psmrts_catch2_environment.hpp>
+
+#include <psmrts/algorithms/conversions/Comparator.hpp>
+#include <psmrts/core/PsmrtsUtilities.hpp>
+#include <psmrts/core/ProductOption.hpp>
+#include <psmrts/core/PsmrtsJson.hpp>
+#include <psmrts/algorithms/conversions/AllConversionsVisitors.hpp>
+
+// Visitor, Extractor, and Comparator types
+using BoolsVisitor    = psmrts::algorithms::conversions::BoolsVisitor;
+using DoublesVisitor  = psmrts::algorithms::conversions::DoublesVisitor;
+using IntegersVisitor = psmrts::algorithms::conversions::IntegersVisitor;
+using SizetsVisitor   = psmrts::algorithms::conversions::SizetsVisitor;
+using StringsVisitor  = psmrts::algorithms::conversions::StringsVisitor;
+
+using BoolsExtractor    = psmrts::algorithms::conversions::Extractor<psmrts::ProductOption, BoolsVisitor>;
+using DoublesExtractor  = psmrts::algorithms::conversions::Extractor<psmrts::ProductOption, DoublesVisitor>;
+using IntegersExtractor = psmrts::algorithms::conversions::Extractor<psmrts::ProductOption, IntegersVisitor>;
+using SizetsExtractor   = psmrts::algorithms::conversions::Extractor<psmrts::ProductOption, SizetsVisitor>;
+using StringsExtractor  = psmrts::algorithms::conversions::Extractor<psmrts::ProductOption, StringsVisitor>;
+
+using BoolsComparator    = psmrts::algorithms::conversions::Comparator<psmrts::ProductOption, BoolsVisitor>;
+using DoublesComparator  = psmrts::algorithms::conversions::Comparator<psmrts::ProductOption, DoublesVisitor>;
+using IntegersComparator = psmrts::algorithms::conversions::Comparator<psmrts::ProductOption, IntegersVisitor>;
+using SizetsComparator   = psmrts::algorithms::conversions::Comparator<psmrts::ProductOption, SizetsVisitor>;
+using StringsComparator  = psmrts::algorithms::conversions::Comparator<psmrts::ProductOption, StringsVisitor>;
+
+
+TEST_CASE("Comparator Default Test", "[comparators][default]") {
+
+  // default comparator constructors
+  BoolsComparator    boolsComp;
+  DoublesComparator  doublesComp;
+  IntegersComparator intsComp;
+  SizetsComparator   sizetsComp;
+  StringsComparator  stringsComp;
+  
+  // validate sizes
+  CHECK( boolsComp.size()   == 1 );
+  CHECK( doublesComp.size() == 1 );
+  CHECK( intsComp.size()    == 1 );
+  CHECK( sizetsComp.size()  == 1 );
+  CHECK( stringsComp.size() == 1 );
+
+  // validate default ProductOption values
+  CHECK( boolsComp.default_value()   == false );
+//CHECK( doublesComp.default_value() == psmrts::null() );
+  CHECK( intsComp.default_value()    == std::numeric_limits<int>::min() );
+  CHECK( sizetsComp.default_value()  == std::numeric_limits<size_t>::max() );
+  CHECK( stringsComp.default_value() == "" );
+
+
+  CHECK( boolsComp.compare( doublesComp.container() ) == true );
+}
+
+TEST_CASE("Comparator Doubles Test", "[comparators][doubles]") {
+
+  psmrts::ProductOption option_d1( "double", 1.2345678 );
+  psmrts::ProductOption option_d2( "double", 3.14159154 );
+  psmrts::ProductOption option_string( "double", "527.987" );
+
+  DoublesComparator doubleComp;
+
+  // why are these failing?
+  CHECK( doubleComp.compare(option_d1, option_d2) == false );
+  CHECK( doubleComp.compare(option_d1, option_string) == false );
+}
