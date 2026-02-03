4153f8ed6fbf5790a561cd66c6e8ded9fba9bffb
kledmundson <k.l.edmundson@gmail.com>
2026-02-03 10:14:42 -0700

    Added license block to files in psmrts/shapes directory.

--- psmrts/shapes/ply/private/PlyShape.cpp
+++ psmrts/shapes/ply/private/PlyShape.cpp
@@ -1,3 +1,14 @@
+/** This is free and unencumbered software released into the public domain.
+ 
+      OSIRIS-APEX (OSIRIS-APophis EXplorer) by University of Arizona 
+                        is marked with CC0 1.0
+      (https://creativecommons.org/publicdomain/zero/1.0/legalcode.en)
+ 
+The authors of PSMRTS do not claim copyright on the contents of this file.
+For more details about the LICENSE terms and the AUTHORS, you will
+find files of those names at the top level of this repository. **/
+ 
+/* SPDX-License-Identifier: CC0-1.0 */
 
 #include <string>
 
