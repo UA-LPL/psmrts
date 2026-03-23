7e8147c34fba59b8473226e9d3f8d398ba51a56f
Kris J. Becker <kjbecker@arizona.edu>
2026-03-22 21:58:32 -0700

    Enhance unique directory creation
    Added loop to attempt creation of a new unique directory. This is still not gauranteed to succede so if it cannot create a unique directory after a specified number of attempts, currently set at 20, it will return the system temporary directory and ensure it will not be removed upon object destruction.

--- psmrts/core/tests/TemporaryDirectoryFixture.hpp
+++ psmrts/core/tests/TemporaryDirectoryFixture.hpp
@@ -25,27 +25,47 @@ namespace fs = std::filesystem;
  * @brief Provide a temporary directory system for Catch2 testing
  * 
  * This class creates and maintains a directory in the system temp directory for
- * isolated file creation and usage.
+ * isolated file creation and usage. The destructor will delete the directory
+ * from temp directory.
  * 
+ * The default constructor will simply return the system temp directory. The
+ * user is assumed to manage its own files in that directory. 
+ * 
+ * @author Kris J. Becker, University of Arizona
+ * @history 2026-03-22 Kris J. Becker  Original Version
  */
 class TemporaryDirectoryFixture {
   public:
     TemporaryDirectoryFixture( ) : m_dirname( ),
                                    m_keep( false ) {
-      m_dirname = TemporaryDirectoryFixture::sys_tmp_dir();
+      m_dirname = fs::temp_directory_path();
       m_keep = true;
     }
 
+    /**
+     * @brief Construct a new temporary unique directory in systen temp
+     * 
+     * Create a unique temporary directory in the system temporary directory.
+     * This directory will be removed by the constructor and any files
+     * contained in the directory. 
+     * 
+     * A local static counter is appended to the dirbase as a string and
+     * incremented for the next unique directory name.
+     * 
+     * @param dirbase Base name of the directory to create
+     */
     TemporaryDirectoryFixture( const std::string &dirbase ) :
                                m_dirname( ),
                                m_keep( false ) {
       m_dirname = make_tmp_directory( dirbase );
     }
 
+    /** Destructor will remove the temporary directory created by constructor */
     virtual ~TemporaryDirectoryFixture() {
       teardown();
     }
 
+    /** Return the system temporary directory */
     static inline std::string sys_tmp_dir() {
       return ( fs::temp_directory_path().u8string() );
     }
@@ -71,7 +91,8 @@ class TemporaryDirectoryFixture {
 
 
     private:
-      static inline unsigned long m_dir_id = 0;  // Unique dir id
+      static inline unsigned long m_dir_id = 0;  // Unique directory id
+      static inline const size_t MaxDirectories = 20;  // Maximum number tries
       fs::path m_dirname;
       bool     m_keep;
 
@@ -80,9 +101,9 @@ class TemporaryDirectoryFixture {
         // Only remove if it exists
         if ( fs::exists( m_dirname ) ) {
 
-          // Only remove if not being expressly being saved
+          // Only remove if not being expressly being saved. We also do not want
+          // to remove the system temp directory.
           if ( !m_keep  && ( m_dirname != fs::temp_directory_path() ) ) {
-            // std::cout << "Removing dir: " << m_dirname  << std::endl;
             (void) fs::remove_all( m_dirname );
           }
         }
@@ -95,14 +116,19 @@ class TemporaryDirectoryFixture {
        * @param d_tmproot Base directory name of temporary directory
        * @return char*    Returns full path to created temporary directory
        */
-      inline fs::path make_tmp_directory( const std::string &d_tmproot  = "psmrts_tmpdir" ) const {
+      inline fs::path make_tmp_directory( const std::string &d_tmproot  = "psmrts_tmpdir" ) {
         fs::path sys_dir_t = fs::temp_directory_path();
-        fs::path tmpdir_t = sys_dir_t / ( d_tmproot + "_" + std::to_string( m_dir_id++ ) );
-        if ( !fs::create_directory( tmpdir_t ) ) {
-          throw std::runtime_error( "TemporaryDirectoryFixture::make_tmp_directory::Error directory not created: " + tmpdir_t.u8string() );
+        for ( size_t ndirs = 0 ; ndirs < MaxDirectories ; ndirs++ ) {
+          fs::path tmpdir_t = sys_dir_t / ( d_tmproot + "_" + std::to_string( m_dir_id++ ) );
+          if ( true == fs::create_directory( tmpdir_t ) ) {
+            m_keep = false;
+            return ( tmpdir_t );
+          }
         }
 
-        return ( tmpdir_t );
+        // Could not create a unique directory, return system tmp.
+        m_keep = true;
+        return ( fs::temp_directory_path() );
       }
 
   };
