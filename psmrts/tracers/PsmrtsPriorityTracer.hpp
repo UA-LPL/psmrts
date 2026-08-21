e756d1c7ec547c683356e4c4fa853fd1fd48ac28
Kris J. Becker <kjbecker@arizona.edu>
2026-08-21 10:24:06 -0700

    PsmrtsPriorityTracer was modified for shared use
    The PsmrtsPriorityTracer was modified to use shared instances of tracers for efficiency and effective use of cache resources.

--- psmrts/tracers/PsmrtsPriorityTracer.hpp
+++ psmrts/tracers/PsmrtsPriorityTracer.hpp
@@ -23,7 +23,7 @@ find files of those names at the top level of this repository. **/
 #include <psmrts/core/PsmrtsProduct.hpp>
 #include <psmrts/core/PsmrtsRayTrace.hpp>
 #include <psmrts/core/PsmrtsRequest.hpp>
-#include <psmrts/core/products/ProductInventory.hpp>
+#include <psmrts/core/PsmrtsInventory.hpp>
 #include <psmrts/tracers/PsmrtsTracer.hpp>
 
 namespace psmrts {
@@ -31,9 +31,10 @@ namespace psmrts {
   class PsmrtsPriorityTracer : public PsmrtsProduct {
     public:
       using UIDType         = PsmrtsUID::UIDType;
-      using TracerList      = std::vector<PsmrtsTracer>;
       using TracerUIDList   = std::vector<UIDType>;
-      using TracerInventory = ProductInventory<UIDType, PsmrtsTracer>;
+      using TracerInventory = PsmrtsInventory::TracerInventory;
+      using SharedTracer    = TracerInventory::SharedType;
+      using TracerList      = std::vector<SharedTracer>;
       using PriorityFunc    = std::function<TracerList(const TracerList &current,
                                                        const TracerInventory &inventory)>;
 
@@ -41,33 +42,38 @@ namespace psmrts {
       PsmrtsPriorityTracer( ) : PsmrtsProduct("prioritytracer") { init(); }
       PsmrtsPriorityTracer( const std::string &name ) : PsmrtsProduct( name ) { init(); }
       
-      PsmrtsPriorityTracer( const PsmrtsTracer &tracer,
+      PsmrtsPriorityTracer( const SharedTracer &tracer,
                             const std::string &name = "" ) : 
                             PsmrtsProduct( name ) { 
         init();
-        if ( name.length() == 0 ) set_name( tracer.name() );
+        if ( !tracer ) return;
+        if ( name.length() == 0 ) set_name( tracer->name() );
         this->add_tracer( tracer );
       }
 
-      virtual ~PsmrtsPriorityTracer() { }
+      virtual ~PsmrtsPriorityTracer() = default;
 
       /** Returns validity state of Priority Tracer, if empty or not */
       inline bool isValid() const {
-        return ( this->size() > 0 );
+        return ( m_tracers.size() > 0 );
       }
 
       /** Returns size of Priority Tracer list */
       inline size_t size() const {
-        return ( tracers().size() );
+        return ( m_tracers.size() );
       }
 
       /** Adds a valid tracer to Priority Tracer list */
-      inline void add_tracer( const PsmrtsTracer &tracer ) {
-        if ( PsmrtsUID::is_valid_uid( tracer.uid() ) ) {
-          m_uids_t.push_back( tracer.uid() );
+      inline void add_tracer( const SharedTracer &tracer ) {
+
+        // Sanity check
+        if ( !tracer ) return;
+
+        if ( PsmrtsUID::is_valid_uid( tracer->uid() ) ) {
+          m_uids_t.push_back( tracer->uid() );
           m_tracers.push_back( tracer );
-          if ( !m_inventory_t.contains( tracer.uid() ) ) {
-            m_inventory_t.add_product( tracer );
+          if ( !m_inventory_t.contains( tracer->uid() ) ) {
+            m_inventory_t.add( tracer );
           }
         }
       }
@@ -75,7 +81,7 @@ namespace psmrts {
       inline bool process ( PRQRayTrace &ray ) const {
         // Trace through list as ordered in the current UID set
         for ( const auto &tracer : tracers() ) {
-          if ( tracer.process( ray )  ) {
+          if ( tracer->process( ray )  ) {
             return ( ray.isValid() );
           }
         }
@@ -87,7 +93,7 @@ namespace psmrts {
         size_t ngood = 0;
         for ( auto &ray : tracelist.traces() ) {
           for ( const auto &tracer : tracers() ) {
-            if ( tracer.process( ray ) == true ) {
+            if ( tracer->process( ray ) == true ) {
               ngood++;
               break;
             }
@@ -97,11 +103,10 @@ namespace psmrts {
       }     
       
 
-
      inline bool process ( PRQPhotometricTrace &ray_p ) const {
         // Trace through list as ordered in the current UID set
         for ( const auto &tracer : tracers() ) {
-          if ( tracer.process( ray_p ) == true ) return ( true );
+          if ( tracer->process( ray_p ) == true ) return ( true );
         }
         return ( false ); // Not a one intercepted
       }     
@@ -112,7 +117,7 @@ namespace psmrts {
         size_t ngood = 0;
         for ( auto &ray : tracelist.traces() ) {
           for ( const auto &tracer : tracers() ) {
-            if ( tracer.process( ray ) == true ) {
+            if ( tracer->process( ray ) == true ) {
               ngood++;
               break;
             } 
@@ -129,36 +134,21 @@ namespace psmrts {
       /**
        * @brief Ray Trace method for tracers in Priority Tracer list
        * 
-       * This method is used to run a body-fixed ray trace from an observer point and 
-       * look direction vector. The origin of the "observer" vector is the origin of the 
-       * planet body and presumeably extends outward beyond the maximum radius of the surface
-       * of the models. From that point, is the origin of the "lookdir" vector from which to 
-       * trace for an intersection with the shape models' surfaces. 
-       * 
        * The method will return the first tracer model in the Priority list that has a hit
        * using the corresponding trace.
        * 
        * The PsmrtsRayTrace class contains the results of the ray trace and can be used in
        * subsequent operations.
        * 
-       * @param observer                    Location of the observer relative to the center
-       *                                     of the target body
-       * @param lookdir                     Look direction of the ray from the observer to 
-       *                                     trace for intersections
-       * @param ray                         PsmrtsRayTrace returns the results of the trace
+       * @param ray PsmrtsRayTrace returns the results of the trace
        */
-      inline bool ray_trace( const PsmrtsTracer &tracer, PRQRayTrace &ray ) const {
-        return ( tracer.process( ray ) );
-      }
-
-      /** Run a trace on the priority list */
       inline bool ray_trace( PRQRayTrace &ray ) const {
-        for ( const auto &tracer : tracers() ) {
-          if ( this->ray_trace( tracer, ray ) == true ) {
+        for ( const auto &tracer : m_tracers ) {
+          if ( tracer->process( ray ) == true ) {
             return ( ray.isValid() );
           }
         }
-        return ( ray.isValid() );
+        return ( false );
       }
 
       /** Return list of tracer uids contained in the inventory */
@@ -178,17 +168,17 @@ namespace psmrts {
 
       /** Find and return pointer to tracer in list, otherwise an invalid
        * tracer is returned */
-      inline PsmrtsTracer get_tracer( const UIDType &uid ) const {
+      inline SharedTracer get_tracer( const UIDType &uid ) const {
         if ( m_inventory_t.contains( uid ) ) {
           return( m_inventory_t.find( uid ) );
         }
 
-        // Return an invalid tracer
-        return ( PsmrtsTracer( "invalid" ) );
+        // Return an invalid shared tracer
+        return ( nullptr );
       }
 
       /** Find the tracer of a valid ray trace result */
-      inline PsmrtsTracer get_tracer( const PsmrtsRayTrace &ray ) const {
+      inline SharedTracer get_tracer( const PsmrtsRayTrace &ray ) const {
         return ( get_tracer( ray.get_tracer_id() ) );
       }
 
@@ -234,7 +224,7 @@ namespace psmrts {
           reversed.reserve( current_order.size() );
           std::transform( current_order.rbegin(), current_order.rend(), 
                           std::back_insert_iterator( reversed ),
-                         []( const PsmrtsTracer &t ) { return ( t ); } );
+                         []( const SharedTracer &t ) { return ( t ); } );
           return ( reversed );
         };
        
@@ -246,9 +236,9 @@ namespace psmrts {
         double max_r ( psmrts::null() );
 
         if ( m_tracers.size() > 0 ) {
-          max_r = m_tracers[0].maximum_radius();
+          max_r = m_tracers[0]->maximum_radius();
           for ( size_t ith = 1 ; ith < m_tracers.size() ; ith++ ) {
-            double radius_t =  m_tracers[ith].maximum_radius();
+            double radius_t =  m_tracers[ith]->maximum_radius();
             if ( radius_t > max_r ) max_r = radius_t;
           }
         }
@@ -262,7 +252,7 @@ namespace psmrts {
         if ( m_tracers.size() > 0 ) {
           min_r = m_tracers[0].minimum_radius();
           for ( size_t ith = 1 ; ith < m_tracers.size() ; ith++ ) {
-            double radius_t = m_tracers[ith].minimum_radius();
+            double radius_t = m_tracers[ith]->minimum_radius();
             if ( radius_t < min_r ) min_r = radius_t;
           }
         }
